326cf7e50f6f2ceef1d234be3860facc2873ef99
balcarcelmar <maryangela140505@gmail.com>
2025-05-28 01:11:53 -0500

    feat: flujo

---
+++ Assets/Main menu with parallax FREE/Scripts/EscenasManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscenasManager : MonoBehaviour
+{
+    [SerializeField] private GameObject panelMenu;
+    [SerializeField] private GameObject panelCreditos;
+
+    public void Game()
+    {
+        SceneManager.LoadScene("FightScene");
+    }
+
+    public void Creditos()
+    {
+        panelMenu.SetActive(false);
+        panelCreditos.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void CreditosToMenu()
+    {
+        panelCreditos.SetActive(false);
+        panelMenu.SetActive(true);
+    }
+}
